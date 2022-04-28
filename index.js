68a68f4f52e504675c4f45309c0abd6756b39e0e
MdMuzahid07 <MdMuzahid7396@gmail.com>
2022-04-29 01:17:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,176 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const res = require('express/lib/response');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if(!authHeader) {
+        return res.status(401).send({message: 'unauthorized access'});
+    }
+
+
+
+    // verify user token
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err) {
+            return res.status(403).send({message: 'Forbidden access'});
+        }
+
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+
+
+}
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.c3izk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('geniusCar').collection('geniusCarServices');
+        const orderCollection = client.db('geniusCar').collection('order');
+    
+
+
+        // auth 
+        app.post('/login', async(req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+
+            res.send({accessToken})
+        })
+
+
+
+
+
+
+
+        // services api
+        app.get('/service', async(req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+
+            res.send(services);
+        });
+
+
+
+        // get one service by id
+
+
+        app.get('/service/:id', async(req, res) => {
+            const id = req.params.id;
+
+            const query = {_id: ObjectId(id)};
+
+            const service = await serviceCollection.findOne(query);
+
+            res.send(service);
+        });
+
+
+        // post data from client side
+
+        app.post('/service', async(req, res) => {
+            const newService = req.body;
+
+
+            const result = await serviceCollection.insertOne(newService);
+
+            res.send(result);
+
+        });
+
+
+        // delete data from client side
+
+        app.delete('/service/:id', async(req, res) =>{
+            const id = req.params.id;
+            console.log(id)
+            const query = {_id: ObjectId(id)};
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+
+        // order collection api
+
+
+        // ---------------------
+
+        app.get('/order', verifyJWT, async (req, res) => {
+
+            const decodedEmail = req.decoded.email;
+
+            const email = req.query.email;
+          
+            if(email === decodedEmail) {
+                const query = {email: email};
+                const cursor = orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            }
+            else {
+                res.status(403).send({message: 'forbidden access'});
+            }
+        })
+
+
+
+        // CURD - C
+
+        app.post('/order', async(req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
+
+
+    }
+    finally {
+
+    }
+};
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Running genius server ??????')
+});
+
+app.listen(port ,() => {
+    console.log('Listening to port??///*****', port)
+})
+
+
